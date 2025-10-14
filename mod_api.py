fb9e1a51bcd3a2224dc4d31248a41d9b840c9940
Haggen So <haggen@haggenso.org>
2025-10-14 02:21:26 +0000

    Add data table

--- mod_api.py
+++ mod_api.py
@@ -6,6 +6,8 @@ def api(mysql, api_type):
 		return test()
 	elif api_type == 'daily_update':
 		return daily_update(mysql)
+	elif api_type == 'source_update':
+		return 	source_update(mysql)
 	else:
 		return jsonify({"error": "api_type error"}), 404
 
@@ -15,6 +17,7 @@ def test():
 
 def daily_update(mysql):
 	param = request.get_json()
+	cursor = mysql.connection.cursor()
 
 	xy = {}
 
@@ -24,8 +27,6 @@ def daily_update(mysql):
 	for _ in range(param['days']):
 		xy[date_count.strftime('%Y-%m-%d')] = 0
 		date_count += timedelta(days=1)
-
-	cursor = mysql.connection.cursor()
 
 	query = "SELECT date(date_recorded), count(scenario_id) FROM scenarios "
 	query += "WHERE date_recorded > DATE_SUB(NOW(), INTERVAL %s DAY) "
@@ -38,6 +39,37 @@ def daily_update(mysql):
 		xy[tmp] = item[1]
 	data = {"x": list(xy.keys()), "y": list(xy.values())}
 	return jsonify(data), 201
+
+def source_update(mysql):
+	cursor = mysql.connection.cursor()
+
+	tbl = []
+
+	query = "SELECT tot_table.sid, tot_table.so_title, valid_table.validated, tot_table.total FROM "
+	query += "(SELECT so.source_id as sid, so.title as so_title, count(scenario_id) as total "
+	query += "FROM scenarios sc, sources so WHERE sc.source_id = so.source_id "
+	query += "AND deleted = 0 "
+	query += "GROUP BY sc.source_id) AS tot_table LEFT JOIN "
+	query += "(SELECT so.source_id as sid, count(scenario_id) as validated "
+	query += "FROM scenarios sc, sources so WHERE sc.source_id = so.source_id "
+	query += "AND deleted = 0 AND sc.validated=1 "
+	query += "GROUP BY sc.source_id) AS valid_table ON "
+	query += "tot_table.sid = valid_table.sid ORDER BY tot_table.sid"
+	cursor.execute(query)
+
+	q_res = cursor.fetchall()
+	for item in q_res:
+		rec = {}
+		rec['sid'] = item[0]
+		rec['title'] = item[1]
+		if item[2]:
+			rec['validate'] = item[2]
+		else:
+			rec['validate'] = 0
+		rec['total'] = item[3]
+		rec['complete'] = round(float(rec['validate']) / float(item[3]) * 100, 2)
+		tbl.append(rec)
+	return jsonify(tbl), 201
 
 """
 select count(scenario_id) from scenarios s
